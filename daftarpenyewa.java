8cc240276b33b9ace21f3323a9984e5e7c280f4e
aanfachrulabid <30574553+aanfachrulabid@users.noreply.github.com>
2018-05-12 18:44:09 +0700

    end

---
+++ daftarpenyewa.java
@@ -0,0 +1,17 @@
+package com.example.aanfa.sewaaja;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class daftarpenyewa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftarpenyewa);
+    }
+
+    public void penyewa1(View view) {
+    }
+}
